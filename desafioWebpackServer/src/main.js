b53f20adec2ac6de1adcdb047b9a464eec6ccaec
Lucas Rinaldi Medeiros Silva <lrmedeiros10@gmail.com>
2020-03-29 22:00:00 -0300

    Modulo 2 do desafio rockeatseat curso de js

---
+++ desafioWebpackServer/src/main.js
@@ -0,0 +1,31 @@
+/**
+1º exercício
+Crie um arquivo chamado functions.js com o seguinte conteúdo:
+
+export const idade = 23;
+export default class Usuario {
+    static info() {
+        console.log('Apenas teste');
+    }
+}*/
+
+/*
+1.1
+Agora em seu arquivo principal importe apenas a classe Usuario renomeando-a para ClasseUsuario
+e chame a função info();*/
+
+import ClasseUsuario from './functions';
+ClasseUsuario.info();
+
+/*
+1.2
+Em seu arquivo principal importe a variável de idade e exiba a mesma em tela;*/
+
+import { idade } from './functions';
+console.log(idade);
+
+/*
+1.3
+Em seu arquivo principal importe tanto a classe quanto a variável idade e renomeie a variável idade para IdadeUsuario.*/
+
+import Usuario, { idade as IdadeUsuario } from './functions';
