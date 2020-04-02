1007bb10424ddc5a64ea854d79e9feb97477586e
Lucas Rinaldi Medeiros Silva <lrmedeiros10@gmail.com>
2020-04-01 22:10:47 -0300

    Finalizando curso es6 site busca users no git

--- Aulas/src/main.js
+++ Aulas/src/main.js
@@ -1,15 +1,90 @@
-import axios from 'axios';
+import api from './api';
 
-class Api {
-    static async getUserInfo(username) {
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        } catch(err) {
-            console.warn('Erro na Api');
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=user]');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addUser(event);
+    }
+
+    setLoading(loading = true) {
+        if(loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addUser(event) {
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0)
+            return;
+
+        this.setLoading();
+        
+        try {
+            const response = await api.get(`/users/${repoInput}`);
+
+            const { login, bio, html_url, avatar_url } = response.data;
+
+            this.repositories.push({
+                login,
+                bio,
+                avatar_url,
+                html_url,
+            });
+            this.inputEl.value = '';
+
+            this.render();
+        } catch (err) {
+            alert('O usuário não existe');
         }
 
+        this.setLoading(false);
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.login));
+
+            let bioEl = document.createElement('p');
+            bioEl.appendChild(document.createTextNode(repo.bio));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(bioEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
-Api.getUserInfo('diego3g');
-Api.getUserInfo('foiasdjfoas');
+
+new App();
